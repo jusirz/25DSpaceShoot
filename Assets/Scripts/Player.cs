2ce25e50809a91c61dbac3c9c01a597f404190a8
jusirz <143116487+jusirz@users.noreply.github.com>
2023-09-04 09:46:16 -0400

    PLayer movements and scripting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position( 0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(_speed * horizontalInput * Time.deltaTime * Vector3.right);
+
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(_speed * Time.deltaTime * verticalInput * Vector3.up);
+    }
+}
